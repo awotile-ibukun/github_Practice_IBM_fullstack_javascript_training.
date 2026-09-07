27c386a2c645b40ba5eaf3fede47801c70953c8a
Awotile Ibukun <ibukunawotile@gmail.com>
2026-09-07 12:45:49 +0100

    Add additional print statement to firstpython.py
    Add additional print statement to firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,5 +1,6 @@
-#Display output, 
+#Display output 
 #This is actually the first Python file I have created in my programming journey
 # I am a JavaScript developer, so I have not created a Python file before.
 
 print("This is my first python string");
+print("I Added another statement to the python file");
