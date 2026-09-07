0a16cc4a05ecde2a0e75c3bd3e8b79f7c8a1c9b8
Awotile Ibukun <ibukunawotile@gmail.com>
2026-09-07 12:43:50 +0100

    Create firstpython.py with a print statement
    Add initial Python script with a print statement.
    this is my first python file

---
+++ firstpython.py
@@ -0,0 +1,5 @@
+#Display output, 
+#This is actually the first Python file I have created in my programming journey
+# I am a JavaScript developer, so I have not created a Python file before.
+
+print("This is my first python string");
